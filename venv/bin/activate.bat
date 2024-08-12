5ee990966951e63266942833599eed5281436bb0
Kashish Gupta <guptakashishiit40@gmail.com>
2024-08-12 18:16:36 +0530

    bin

--- venv/bin/activate.bat
+++ venv/bin/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91787\App_dev\lit\venv
+set VIRTUAL_ENV=C:\Users\91787\lit-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
