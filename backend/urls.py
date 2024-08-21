23dcc364b68756d03372c5236c541b8e7b631f2c
Kashish Gupta <guptakashishiit40@gmail.com>
2024-08-21 20:59:39 +0530

    save

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import home,signup_view, login_view, UserList, UserDetail
+from .views import home,signup, login, UserList, UserDetail
 
 urlpatterns = [
      path('', home, name='home'), 
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
     path('api/users/', UserList.as_view(), name='user-list'),
     path('api/users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
 ]
