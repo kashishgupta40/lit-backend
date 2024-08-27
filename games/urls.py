3925d1453ec54c605b0126107121ff5bf648ac71
Kashish Gupta <guptakashishiit40@gmail.com>
2024-08-27 23:55:04 +0530

    admin registered and a test_api page created

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProductViewSet, GameSessionViewSet, index
+from .views import test_game ,UserViewSet, ProductViewSet, GameSessionViewSet, index
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,5 +9,7 @@ router.register(r'gamesessions', GameSessionViewSet)
 
 urlpatterns = [
     path('', index, name='index'),
+    path('', include(router.urls)),
+    path('test_game/', test_game, name='test_game'),
     path('api/', include(router.urls)),
 ]
--- games/views.py
+++ games/views.py
@@ -18,3 +18,22 @@ class GameSessionViewSet(viewsets.ModelViewSet):
 
 def index(request):
     return render(request, 'games/index.html')
+
+def test_game(request):
+    result = None
+    if request.method == 'POST':
+        user_name = request.POST.get('user')
+        product1_name = request.POST.get('product1')
+        product2_name = request.POST.get('product2')
+
+        user = User.objects.get(username=user_name)
+        product1 = Product.objects.get(name=product1_name)
+        product2 = Product.objects.get(name=product2_name)
+
+        session = GameSession.objects.create(user=user, product1=product1, product2=product2)
+        session.correct_answer = product1.price > product2.price
+        session.save()
+
+        result = session.correct_answer
+
+    return render(request, 'games/test_game.html', {'result': result})
