c7084f781a1d6797ac491f44e7003c62b264d525
shivankupadhyay-eng <shivankupadhyay51@gmail.com>
2024-08-27 14:22:51 +0530

    removed email field

--- backend/serializers.py
+++ backend/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password' ]
+        fields = ['username', 'password' ]
 
     def create(self, validated_data):
         user = User.objects.create_user(
