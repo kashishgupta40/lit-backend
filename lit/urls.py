ecd86a4bcf99b26de2895c4f58ad4364944936c7
Kashish Gupta <guptakashishiit40@gmail.com>
2024-11-29 21:37:22 +0530

    bearere token url added

--- lit/urls.py
+++ lit/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
     path('game/', include('games.urls')),
     path('store/', include('store.urls')),
+    path('api/token/', obtain_auth_token, name='api_token'),
 ]
--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-
     # Django REST API views
     path('api/cart/add/<int:product_id>/', views.api_add_to_cart, name='api_add_to_cart'),
     path('api/cart/remove/<int:product_id>/', views.api_remove_from_cart, name='api_remove_from_cart'),
